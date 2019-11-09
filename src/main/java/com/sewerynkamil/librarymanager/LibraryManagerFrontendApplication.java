d5b6805229f8f671fb2e5447a7f062084ceaac46
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-11-09 20:07:11 +0100

    Initial commit

---
+++ src/main/java/com/sewerynkamil/librarymanager/LibraryManagerFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.sewerynkamil.librarymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagerFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sewerynkamil/librarymanager/LibraryManagerFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sewerynkamil.librarymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
