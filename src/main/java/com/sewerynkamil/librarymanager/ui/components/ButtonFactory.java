05fc6b67f90689d04a33e0e42d39cd37ac8d79a9
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-11-29 20:08:05 +0100

    Update ButtonFactory - delete useless code

--- src/main/java/com/sewerynkamil/librarymanager/ui/components/ButtonFactory.java
+++ src/main/java/com/sewerynkamil/librarymanager/ui/components/ButtonFactory.java
@@ -9,7 +9,6 @@ import com.vaadin.flow.component.icon.VaadinIcon;
  */
 
 public class ButtonFactory {
-
     private Button button;
 
     public final Button createButton(ButtonType type, String buttonName, String width) {
@@ -19,11 +18,6 @@ public class ButtonFactory {
                 button.setWidth(width);
                 return button;
             case SAVE:
-                button = new Button(buttonName, VaadinIcon.CHECK.create());
-                button.setWidth(width);
-                button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-                //saveButton.getElement().getThemeList().add("primary");
-                return button;
             case UPDATE:
                 button = new Button(buttonName, VaadinIcon.CHECK.create());
                 button.setWidth(width);
@@ -33,13 +27,11 @@ public class ButtonFactory {
                 Button deleteButton = new Button(buttonName, VaadinIcon.TRASH.create());
                 deleteButton.setWidth(width);
                 deleteButton.addThemeVariants(ButtonVariant.LUMO_ERROR, ButtonVariant.LUMO_PRIMARY);
-                //deleteButton.getElement().getThemeList().add("error");
                 return deleteButton;
             case RESET:
                 Button resetButton = new Button(buttonName, VaadinIcon.ROTATE_LEFT.create());
                 resetButton.setWidth(width);
                 resetButton.addThemeVariants(ButtonVariant.LUMO_SUCCESS, ButtonVariant.LUMO_PRIMARY);
-                //resetButton.getElement().getThemeList().add("success");
                 return resetButton;
             case CLOSE:
                 Button closeButton = new Button(buttonName, VaadinIcon.CLOSE.create());
